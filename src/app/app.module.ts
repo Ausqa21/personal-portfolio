3ed0a79245931dc14864b3b19ba5715b3641dd51
dev_godric <godolphine21@gmail.com>
2021-12-27 12:43:54 +0000

    Added components: work, contact, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { WorkComponent } from './components/work/work.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    WorkComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
